361788acffec90f5f47373e5eba71ce095f2dca7
theM11 <jon.miroslav@gmail.com>
2020-09-26 10:46:51 +0200

    update of credentials, now are configured from FTP - development center preview

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Yello World
+          test
         </p>
         <a
           className="App-link"
